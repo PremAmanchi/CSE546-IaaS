7e2edd6b98cf785ea60ea178b89e383ffbf78206
Prem Kumar Amanchi <premkumaramanchi05@gmail.com>
2023-10-01 00:50:01 -0700

    complete sqs app-tier s3 local pipeline

--- APP-TIER/controller/sqs_send_app_tier.js
+++ APP-TIER/controller/sqs_send_app_tier.js
@@ -1,11 +1,11 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 // const shell = require('shelljs')
-AWS.config.update({ region: 'us-east-1' });
-var SQS = new AWS;.SQS({
-    accessKeyId: "AKIAX7SHWYTIJIMWZGNT",
+AWS.config.update({ region: "us-east-1" });
+var SQS = new AWS.SQS({
+  accessKeyId: "AKIAX7SHWYTIJIMWZGNT",
   secretAccessKey: "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",
 });
-const fs = require('fs');
+const fs = require("fs");
 const BUCKET_NAME = "input-images-cse546";
 
 const S3 = new AWS.S3({
@@ -13,73 +13,60 @@ const S3 = new AWS.S3({
   secretAccessKey: "+IyMj0veGD7ByGhpNtnRuARjy3kzQz9ujiMHnlTU",
 });
 
-const responseQueueURL = "https://sqs.us-east-1.amazonaws.com/548832462032/CSE-546-PROJECT-1-RESPONSE-QUEUE";
+const responseQueueURL =
+  "https://sqs.us-east-1.amazonaws.com/548832462032/CSE-546-PROJECT-1-RESPONSE-QUEUE";
 
 const sendMessage = (output) => {
-    var params = {
-        MessageAttributes: {
-            'output': {
-                DataType: "String",
-                StringValue: output
-            }
-        },
-        MessageBody: "SQS Response.",
-        QueueUrl: responseQueueURL
-    };
+  var params = {
+    MessageAttributes: {
+      output: {
+        DataType: "String",
+        StringValue: output,
+      },
+    },
+    MessageBody: "SQS Response.",
+    QueueUrl: responseQueueURL,
+  };
 
-    SQS.sendMessage(params, function (err, data) { 
-        if (err) {
-            console.log("Error : ", err);
-        }
-        else {
-            console.log("Success", data.MessageId);
-        }
-    });
-}
-
-const uploadFile = (fileName) => {
-    fs.readFile(fileName, (err, data) =>){
-        if (err) throw err;
-        const params = {
-            Bucket: BUCKET_NAME,
-            Key: fileName,
-            Body: data
-        };
-
-        S3.upload(params, function (err, data) {
-            if (err) throw err;
-        });
-    };
+  SQS.sendMessage(params, function (err, data) {
+    if (err) {
+      console.log("Error : ", err);
+    } else {
+      console.log("Success", data.MessageId);
+    }
+  });
 };
 
+const uploadFile = (fileName) => {
+  fs.readFile(fileName, (err, data) => {
+    if (err) throw err;
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: fileName,
+      Body: data,
+    };
 
-fs.readFile('output.txt', 'utf-8', (err, data) => {
-    
-    key = data.split('#')[0]
-    key = key.split('/')[1]
-    value = data.split('#')[1]
+    S3.upload(params, function (err, data) {
+      if (err) throw err;
+    });
+  });
+};
+
+fs.readFile("output.txt", "utf8", (err, data) => {
+    console.log(data);
+    key = data.split('#')[0];
+
+    value = data.split("#")[1];
     value = value.replace("\n", "").replace("\r", "");
-    file_content = '('+key+','+value+')'
-    fileName = key.split('.')[0] + '.txt'
-    fs.writeFile(filename, file_content, function (err) {
-        if (err) throw err;
-    })
-    sendMessage(key + '#' + value);
+    file_content = "(" + key + "," + value + ")";
+    fileName = key.split(".")[0] + ".txt";
+    fs.writeFile(fileName, file_content, function (err) {
+      if (err) throw err;
+    });
+    sendMessage(key + "#" + value);
     uploadFile(fileName);
-    fs.unlinkSync('output.txt');
-    fs.unlinkSync(fileName)
+    fs.unlinkSync("output.txt");
+    fs.unlinkSync(fileName);
 
-    // var receiveParams = {
-    //     AttributeNames: [
-    //         "SentTimestamp"
-    //     ],
-    //     MaxNumberOfMessages: 1,
-    //     MessageAttributeNames: [
-    //         "All"
-    //     ],
-    //     QueueUrl: "https://sqs.us-east-1.amazonaws.com/981802952135/cc-project-sqs-input",
-    //     VisibilityTimeout: 10,
-    //     WaitTimeSeconds: 20
-    // };
-
+    
 });
