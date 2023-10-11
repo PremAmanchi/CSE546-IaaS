6dcbba8c41935ded6bcc441145ed694bdd6dfca5
Prem Kumar Amanchi <premkumaramanchi05@gmail.com>
2023-10-10 19:21:52 -0700

    keep only project files

--- APP-TIER-LOCAL/app_tier.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /Users/premkumaramanchi/CODE/DEV/CSE546-IaaS/APP-TIER-LOCAL/controller
-node request.js
-cd /Users/premkumaramanchi/CODE/DEV/CSE546-IaaS/APP-TIER-LOCAL/classifier
-image_name=$(find ./ -type f \( -iname \*.JPEG -o -iname \*.jpg -o -iname \*.png \))
-python3 image_classification.py $image_name
-cd /Users/premkumaramanchi/CODE/DEV/CSE546-IaaS/APP-TIER-LOCAL/controller
-node response.js
--- APP-TIER-LOCAL/terminate_app_tier.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/CC_Project_App_Tier/controller
-node terminate_instance.js
